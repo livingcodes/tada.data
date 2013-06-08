1a494fc2a47b7c51c18d204ab63231949dd5e71e
Casey Joyce <case.joyce@gmail.com>
2013-06-07 23:47:00 -0500

    struct tests with user

---
+++ code_joys.tadu.tests/tada/struct_tests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tada.tests
+{
+[TestClass]
+public class struct_tests : base_test
+{
+   [TestMethod] public void insert_update_select_delete() {
+      var user = new user();
+      user.email = "insert@update.com";
+      user.password = "password";
+
+      var db = new session();
+
+      user.id = db.insert(user);
+      assert(user.id > 0);
+
+      user.password = "new";
+      var rows_affected = db.update(user);
+      assert(rows_affected == 1);
+
+      user = db.one<user>(user.id);
+      assert(user.password == "new");
+
+      rows_affected = db.delete<user>(user.id);
+      assert(rows_affected == 1);
+
+      // throws exception since user does not exist
+      //user = db.one<user>(user.id);
+      //assert(user.id == 0);
+   }
+}
+}
