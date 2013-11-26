8b416f2aa46767deab39516ddc1c935b1797f445
Winnercomm Desktop <case.joyce@gmail.com>
2013-11-26 15:40:10 -0600

    duplicates

--- code_joys.tadu/code_joys/list_extensions.cs
+++ code_joys.tadu/code_joys/list_extensions.cs
@@ -8,5 +8,19 @@ public static class list_extensions
    public static void add<t>(this List<t> list, params t[] items) {
       list.AddRange(items);
    }
+
+   public static List<t> duplicates<t>(this List<t> list,
+      Func<t, t, bool> comparison
+   ) {
+      var duplicates = new List<t>();
+      for (var i=0; i<list.Count-1; i++)
+         for (var j=i+1; j<list.Count; j++)
+            if (i != j)
+               if (comparison(list[i], list[j])) {
+                  duplicates.Add(list[j]);
+                  break;
+               }
+      return duplicates;
+   }
 }
 }
