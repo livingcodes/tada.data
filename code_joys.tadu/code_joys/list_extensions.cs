9735d622579526c5fe8cbb542829cc032eb942fe
Winnercomm Desktop <case.joyce@gmail.com>
2013-11-26 15:44:50 -0600

    list.delimit

--- code_joys.tadu/code_joys/list_extensions.cs
+++ code_joys.tadu/code_joys/list_extensions.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace code_joys
 {
 public static class list_extensions
 {
-   public static void add<t>(this List<t> list, params t[] items) {
+   public static List<t> add<t>(this List<t> list, params t[] items) {
       list.AddRange(items);
+      return list;
+   }
+
+   public static string delimit(this IList list, string delimiter, string conjunction = "") {
+      var result = "";
+      
+      for (var i=0; i<list.Count; i++) {
+         result += list[i];          // a
+         if (i < list.Count-2)
+            result += delimiter;    // "a, "
+         if (i == list.Count-2)
+            result += conjunction == "" ? delimiter : conjunction;  // "a, " or "a and"
+      }
+
+      return result;
    }
 
    public static List<t> duplicates<t>(this List<t> list,
